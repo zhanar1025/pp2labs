d292057c1c2d26b16fd330e838a1b491db381781
unknown <https://github.com/zhanar1025/pp2labs.git>
2018-01-14 00:50:44 +0600

    kek

---
+++ lab1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string line = Console.ReadLine();
+            List<int> mkk = new List<int> ();
+            for(int i = 0; i < args.Length; i++)
+            {
+                mkk.Add(int.Parse(args[i]));
+            }
+            for(int i = 0; i < mkk.Count; i++)
+            {
+                int n = mkk[i];
+                bool prime = true;
+                for(int j = 2; j * j <= n; j++)
+                {
+                    if (n % j == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                if (prime && n!=1)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
