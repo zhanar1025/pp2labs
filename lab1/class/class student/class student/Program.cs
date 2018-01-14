5e18234ccb7d42cc4bf76397b852723edeceb695
unknown <https://github.com/zhanar1025/pp2labs.git>
2018-01-14 15:30:20 +0600

    second

---
+++ lab1/class/class student/class student/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    public class student
+    {
+        public string firstname;
+        public string lastname;
+        public int age;
+        public student()
+        {
+            firstname = "Zhanar";
+            lastname = "Orynbasar";
+            age = 18;
+        }
+        public student(string firstname, string lastname, int age)
+        {
+            firstname = "Zhanar";
+            lastname = "Orynbasar";
+            age = 18;
+        }
+        public void print()
+        {
+            Console.WriteLine("firstname: " + firstname);
+            Console.WriteLine("lastname: " + lastname);
+            Console.WriteLine("age: " + age);
+
+        }
+    }
+        class MainClass
+        {
+        public static void Main(string[] args)
+        {
+            student s1 = new student();
+            s1.print();
+            Console.ReadKey();
+        }
+    }
+}
