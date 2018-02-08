06a5ae21485b65d49051bd2b7c0aacdc398a90e6
unknown <https://github.com/zhanar1025/pp2labs.git>
2018-02-09 01:54:58 +0600

    final

---
+++ lab3/farman/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Show(DirectoryInfo drinf,int cursor)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            FileSystemInfo[] fileinfos = drinf.GetFileSystemInfos();
+            for(int i = 0; i < fileinfos.Length; i++)
+            {
+                FileSystemInfo fileinfo = fileinfos[i];
+                if (i == cursor)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                }
+                else
+                    Console.BackgroundColor = ConsoleColor.Black;
+                if (fileinfo.GetType() == typeof(DirectoryInfo))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                Console.WriteLine(fileinfo.Name);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"C:\Users\Zhanar\Desktop\pp2labs");
+            int cursor = 0;
+            int n = directoryInfo.GetFileSystemInfos().Length;
+            Show(directoryInfo , cursor);
+            while (true)
+            {
+                ConsoleKeyInfo keyinfo = Console.ReadKey();
+                if (keyinfo.Key == ConsoleKey.DownArrow)
+                {
+                    cursor++;
+                    if (cursor == n)
+                        cursor = 0;
+                }
+                if (keyinfo.Key == ConsoleKey.UpArrow)
+                {
+                    cursor--;
+                    if (cursor == -1)
+                        cursor = n-1;
+                }
+                if (keyinfo.Key == ConsoleKey.Enter)
+                {
+                    if (directoryInfo.GetFileSystemInfos()[cursor].GetType() == typeof(DirectoryInfo))
+                    {
+                        directoryInfo = new DirectoryInfo(directoryInfo.GetFileSystemInfos()[cursor].FullName);
+                        cursor = 0;
+                        n = directoryInfo.GetFileSystemInfos().Length;
+                    }
+                    else
+                    {
+                        StreamReader sr = new StreamReader(directoryInfo.GetFileSystemInfos()[cursor].FullName);
+                        string s = sr.ReadToEnd();
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine(s);
+                        Console.ReadKey();
+                    }
+                }
+                if (keyinfo.Key == ConsoleKey.Escape)
+                {
+                    if (directoryInfo.Parent != null)
+                    {
+                        directoryInfo = directoryInfo.Parent;
+                        cursor = 0;
+                        n = directoryInfo.GetFileSystemInfos().Length;
+                    }
+                    else
+                        break;
+                }
+                Show(directoryInfo, cursor);
+            }
+        }
+    }
+}
---
+++ lab4/compalex/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsoleApp1
+{
+    [Serializable]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string c = s.Split(' ')[0];
+            string d = s.Split(' ')[1];
+            int a = int.Parse(c.Split('/')[0]);
+            int b = int.Parse(c.Split('/')[1]);
+            int a1 = int.Parse(d.Split('/')[0]);
+            int b1 = int.Parse(d.Split('/')[1]);
+            complexnum q = new complexnum(a, b);
+            complexnum w = new complexnum(a1, b1);
+            complexnum res = q + w;
+            Console.WriteLine(res);
+           // Console.ReadKey();
+            FileStream fs = new FileStream("data.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs,res);
+            fs.Close();
+            bf = new BinaryFormatter();
+            FileStream fss = new FileStream("data.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            complexnum v = bf.Deserialize(fss) as complexnum;
+            Console.WriteLine(v);
+            Console.ReadKey();
+        }
+    }
+}
